b10395fe1f76d9e683a44f83a2091bcdb7bcdbb8
Sdfeagt <rafalciechanski15@gmail.com>
2022-09-29 19:19:20 +0300

    Further work on header css. Co-authored-by: András Ádám

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import NavBar from './NavBar'
 import "../styles/Header.css"
+import { useNavigate } from "react-router-dom";
+import{useCallback} from 'react';
 
-const Header = () => {
+
+
+const Header = (event) => {
+  const navigate = useNavigate()
+  const handleOnClick = useCallback(() => navigate('/', {replace: true}), [navigate]);
   return (
     <section className='Header'>
-        <section>
-            <section className='Logo'>
-              <img src='logomark.teal__1_-removebg-preview.png' alt='Logo' width={100} height={100}></img>
-            </section>
-            <section className='Nav'><NavBar/></section>
-        </section>
+      <section className='Logo'>
+        <img src='logomark.teal__1_-removebg-preview.png' alt='Logo' width={100} height={100} onClick={handleOnClick}></img>
+      </section>
+      <section className='Nav'><NavBar/></section>
     </section>
   )
 }
