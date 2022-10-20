fbd82475a68189459c51c22fac012d29dfcacfa6
Sdfeagt <rafalciechanski15@gmail.com>
2022-10-20 14:40:19 +0300

    Further work on About page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div className='aboutPage'>
       <div className='Entrance'>
       <h1>About</h1>
-      <p>Some text about how cool we are</p>
+      <p>Our goal is to unite Aalto University students interested in a career in consulting and to create a connection between these students and consulting companies.</p>
       </div>
       <div className='Content'>
       <div className='Topic'>
@@ -16,10 +16,9 @@ const About = () => {
       <div className='textstuff'>
         <div>
         <p>
-        TexttexttextTexttexttextTexttexttextTexttexttextTexttexttextTexttexttext
-        TexttexttextTexttexttextTexttexttextTexttexttextTexttexttextTexttexttext
-        TexttexttextTexttexttextTexttexttextTexttexttextTexttexttextTexttexttext
-        TexttexttextTexttexttextTexttexttextTexttexttextTexttexttextTexttexttext
+        We are a student organization in Aalto University that 
+        aims to support the development of its members in the skills 
+        required for a career in management consulting, and to improve the readiness of members for working life.
         </p>
         </div>
       </div>
