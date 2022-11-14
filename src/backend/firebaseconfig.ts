1bc3efe0893c45db9cb62e032c181d169e7658dc
Sdfeagt <rafalciechanski15@gmail.com>
2022-11-14 12:27:22 +0200

    Fixed dotenv bug, removed unused packages

--- src/backend/firebaseconfig.ts
+++ src/backend/firebaseconfig.ts
@@ -3,13 +3,13 @@ import { getFirestore, collection, addDoc} from 'firebase/firestore'
 
 
 const config: FirebaseOptions = {
-  apiKey: process.env.FIREBASE_API,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASURMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 const app = initializeApp(config);
